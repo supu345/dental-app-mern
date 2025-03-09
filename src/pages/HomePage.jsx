202d2ae3c5b64f9ed89b1caf150b033dda689260
supu345 <supu345@gmail.com>
2025-03-09 11:02:48 +0600

    change in dark theme in testimonial

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -265,14 +265,14 @@ const HomePage = () => {
         </div>
         {/* Testimonial */}
 
-        <p className="text-center text-black text-5xl font-bold">
+        <p className="text-center text-black dark:text-white dark:bg-black text-5xl font-bold">
           Testimonials
         </p>
-        <h1 className="text-xl pt-3 text-center text-black">
+        <h1 className="text-xl pt-3 text-center text-black dark:text-white dark:bg-black pb-5">
           What Our Clients Say
         </h1>
         <div
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-amber-500 gap-6 py-7 px-4 md:px-9 mt-9 w-full"
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-amber-500 gap-6 py-7 px-4 md:px-9 pt-9 w-full dark:text-white dark:bg-black"
           style={{
             backgroundImage:
               "url('https://images.pexels.com/photos/4269365/pexels-photo-4269365.jpeg?auto=compress&cs=tinysrgb&w=600')",
@@ -281,14 +281,14 @@ const HomePage = () => {
           }}
         >
           {/* Testimonial Card */}
-          <div className="flex flex-col items-center mt-2 mb-9 py-6 bg-white h-auto text-center shadow-lg rounded-lg">
+          <div className="flex flex-col items-center mt-2 mb-9 py-6 bg-white  dark:bg-black h-auto text-center shadow-lg rounded-lg">
             <img
               src="https://images.pexels.com/photos/845457/pexels-photo-845457.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
               alt="John Doe"
               className="h-[80px] w-[80px] mx-auto rounded-full"
             />
             <p className="font-bold text-lg md:text-xl mt-4">John Doe</p>
-            <p className="py-4 px-4 text-sm md:text-base">
+            <p className="py-4 px-4 text-sm md:text-base dark:text-white dark:bg-black">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
               quae aliquid molestiae sed illo aut earum alias aspernatur iusto!
               Eveniet.
@@ -296,7 +296,7 @@ const HomePage = () => {
           </div>
 
           {/* Testimonial Card */}
-          <div className="flex flex-col items-center mt-2 mb-9 py-6 bg-white h-auto text-center shadow-lg rounded-lg">
+          <div className="flex flex-col items-center mt-2 mb-9 py-6 bg-white  dark:bg-black h-auto text-center shadow-lg rounded-lg">
             <img
               src="https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
               alt="Fahim Rashid"
@@ -311,7 +311,7 @@ const HomePage = () => {
           </div>
 
           {/* Testimonial Card */}
-          <div className="flex flex-col items-center mt-2 mb-9 py-6 bg-white h-auto text-center shadow-lg rounded-lg">
+          <div className="flex flex-col items-center mt-2 mb-9 py-6 bg-white  dark:bg-black h-auto text-center shadow-lg rounded-lg">
             <img
               src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
               alt="Jane Smith"
