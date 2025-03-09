4a9063c33a6a44aad7bf5a40f361e096e121d2f5
supu345 <supu345@gmail.com>
2025-03-09 11:09:16 +0600

    change in footer

--- src/components/layout/CreateAt.jsx
+++ src/components/layout/CreateAt.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const CreateAt = () => {
   return (
-    <div className="dark:bg-black dark:text-white pb-7 mt-7">
+    <div className="dark:bg-black dark:text-white pb-7 pt-7">
       <div className="flex flex-row justify-center gap-5 px-[100px]">
         <div className="flex flex-row gap-4">
           <p>
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,16 +3,16 @@ import { FaFacebookF } from "react-icons/fa";
 import { IoCall } from "react-icons/io5";
 const Footer = () => {
   return (
-    <div className="py-6 px-6">
+    <div className="py-6 px-6 dark:bg-black dark:text-white">
       <div className="flex flex-col md:flex-row pt-9 gap-6">
         <div className=" w-full md:w-1/4">
-          <p className="text-4xl font-extrabold text-center text-slate-600">
+          <p className="text-4xl font-extrabold text-center text-slate-600 dark:bg-black dark:text-white">
             Phone
           </p>
           <p className="text-center mt-7">01999 44 1515 , 01999 55 1616</p>
         </div>
         <div className=" w-full md:w-1/4">
-          <p className="text-4xl font-extrabold text-center text-slate-600">
+          <p className="text-4xl font-extrabold text-center text-slate-600 dark:bg-black dark:text-white">
             Address
           </p>
           <p className="text-center mt-7">
@@ -20,16 +20,16 @@ const Footer = () => {
           </p>
         </div>
         <div className=" w-full md:w-1/4">
-          <p className="text-4xl font-extrabold text-center text-slate-600">
+          <p className="text-4xl font-extrabold text-center text-slate-600 dark:bg-black dark:text-white">
             Email
           </p>
-          <p className="text-center mt-7">example@gmail.com</p>
+          <p className="text-center pt-7">example@gmail.com</p>
         </div>
         <div className=" w-full md:w-1/4">
-          <p className="text-4xl font-extrabold text-center text-slate-600">
+          <p className="text-4xl font-extrabold text-center text-slate-600 dark:bg-black dark:text-white">
             Follow Us
           </p>
-          <div className="flex flex-row gap-5 ml-[200px] md:ml-[120px] mt-7">
+          <div className="flex flex-row gap-5 ml-[200px] md:ml-[120px] pt-7">
             <FaFacebookF className="bg-blue-800 text-white p-1 text-3xl" />
             <IoCall className="bg-blue-800 text-white p-1 text-3xl" />
           </div>
